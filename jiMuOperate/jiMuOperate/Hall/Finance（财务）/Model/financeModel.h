142ee18253ec6ce184f8ea29f32f2dc77ead0517
humeifang <humeifang@betterchain.io>
2018-11-23 11:36:06 +0800

    addTwo file

---
+++ jiMuOperate/jiMuOperate/Hall/Finance（财务）/Model/financeModel.h
@@ -0,0 +1,17 @@
+//
+//  financeModel.h
+//  jiMuOperate
+//
+//  Created by macbook pro on 2018/11/23.
+//  Copyright © 2018年 jinXinFengLi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface financeModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ jiMuOperate/jiMuOperate/Hall/Finance（财务）/Model/financeModel.m
@@ -0,0 +1,13 @@
+//
+//  financeModel.m
+//  jiMuOperate
+//
+//  Created by macbook pro on 2018/11/23.
+//  Copyright © 2018年 jinXinFengLi. All rights reserved.
+//
+
+#import "financeModel.h"
+
+@implementation financeModel
+
+@end
